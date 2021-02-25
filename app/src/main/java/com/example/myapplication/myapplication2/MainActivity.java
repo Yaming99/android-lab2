1c29777aae45c22cfdd4fb47b3b93fc0121a0fbd
QUACH Anthony <anthony.quach@efrei.net>
2021-02-25 23:57:54 +0100

    Ex 3 and 4

--- app/src/main/java/com/example/myapplication/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication/myapplication2/MainActivity.java
@@ -3,6 +3,16 @@ package com.example.myapplication.myapplication2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onLogin(View view) {
+        // surround it in a thread
+        new Thread() {
+            public void run() {
+                URL url;
+                try {
+                    // change to https
+                    url = new URL("https://www.android.com/");
+                    HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+                    // get the content of the page
+                    try {
+                        InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                        String s = readStream(in);
+                        Log.i("JFL", s);
+                    } finally {
+                        urlConnection.disconnect();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
+    private String readStream(InputStream inputStream) {
+        try {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            int i = inputStream.read();
+            // read output stream to its end
+            while(i != -1) {
+                byteArrayOutputStream.write(i);
+                i = inputStream.read();
+            }
+            return byteArrayOutputStream.toString();
+        } catch (IOException e) {
+            return "";
+        }
     }
 }
