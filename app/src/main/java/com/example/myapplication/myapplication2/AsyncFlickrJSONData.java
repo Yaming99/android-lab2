e112df290c0ff0661518300d0ec38446a3e55834
QUACH Anthony <anthony.quach@efrei.net>
2021-02-26 03:29:39 +0100

    Ex 15

--- app/src/main/java/com/example/myapplication/myapplication2/AsyncFlickrJSONData.java
+++ app/src/main/java/com/example/myapplication/myapplication2/AsyncFlickrJSONData.java
@@ -1,11 +1,66 @@
 package com.example.myapplication.myapplication2;
 
 import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
 public class AsyncFlickrJSONData extends AsyncTask<String, Void, JSONObject> {
+    // method similar to the previous one in MainActivity
     @Override
     protected JSONObject doInBackground(String... params) {
-        return null;
+        JSONObject result = null;
+        try {
+            URL url = new URL(params[0]);
+            // connection to the url
+            HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+            try {
+                // get the data from it
+                InputStream is = new BufferedInputStream(urlConnection.getInputStream());
+                String s = readStream(is);
+                // reinstantiate the data as a JSONObject
+                result = new JSONObject(s);
+            } finally {
+                urlConnection.disconnect();
+            }
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
+
+    @Override
+    protected void onPostExecute(JSONObject result) {
+        try {
+            Log.i("JFL", result.toString(4));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // same method as before
+    private String readStream(InputStream inputStream) {
+        try {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            int i = inputStream.read();
+            // read output stream to its end
+            while(i != -1) {
+                byteArrayOutputStream.write(i);
+                i = inputStream.read();
+            }
+            return byteArrayOutputStream.toString();
+        } catch (IOException e) {
+            return "";
+        }
+    }
+
 }
--- app/src/main/java/com/example/myapplication/myapplication2/GetImageOnClickListener.java
+++ app/src/main/java/com/example/myapplication/myapplication2/GetImageOnClickListener.java
@@ -5,6 +5,6 @@ import android.view.View;
 public class GetImageOnClickListener implements View.OnClickListener {
     @Override
     public void onClick(View v) {
-        new AsyncFlickrJSONData().execute("https://www.flickr.com/services/feeds/photos_public.gne?tags=trees&format=json");
+        new AsyncFlickrJSONData().execute("https://www.flickr.com/services/feeds/photos_public.gne?tags=trees&format=json&nojsoncallback=?");
     }
 }
