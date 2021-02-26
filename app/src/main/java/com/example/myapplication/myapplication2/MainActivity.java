f1aa632cf74e82c36db0fecbac6de863358e4e01
QUACH Anthony <anthony.quach@efrei.net>
2021-02-26 04:41:25 +0100

    Ex 19

--- app/src/main/java/com/example/myapplication/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication/myapplication2/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.myapplication.myapplication2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -16,5 +18,12 @@ public class MainActivity extends AppCompatActivity {
         // getting the img btn
         Button getImage = findViewById(R.id.getImgBtn);
         getImage.setOnClickListener(new GetImageOnClickListener(imageView));
+    }
+
+    // get the list activity
+    public void seeList(View view) {
+        // new activity
+        Intent list = new Intent(getApplicationContext(), ListActivity.class);
+        startActivity(list);
     }
 }
