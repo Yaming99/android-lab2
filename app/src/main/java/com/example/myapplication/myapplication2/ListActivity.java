7f1d63385b3b5ca431fe7f849f237e713ce6afb3
QUACH Anthony <anthony.quach@efrei.net>
2021-02-26 04:48:13 +0100

    Ex 20

--- app/src/main/java/com/example/myapplication/myapplication2/ListActivity.java
+++ app/src/main/java/com/example/myapplication/myapplication2/ListActivity.java
@@ -3,12 +3,18 @@ package com.example.myapplication.myapplication2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class ListActivity extends AppCompatActivity {
+    private MyAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        // link the list
+        adapter = new MyAdapter();
+        ListView list = findViewById(R.id.list);
+        list.setAdapter(adapter);
     }
 }
